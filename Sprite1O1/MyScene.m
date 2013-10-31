82f4e4c98f2652d23eed14b004568ba10e6e1bb8
salimos <socom.skx@gmail.com>
2013-11-01 00:31:15 +0100

    add fucking monsters :]

--- Sprite1O1/MyScene.m
+++ Sprite1O1/MyScene.m
@@ -24,11 +24,57 @@
         
         // 4
         self.player = [SKSpriteNode spriteNodeWithImageNamed:@"player"];
-        self.player.position = CGPointMake(100, 100);
+        self.player.position = CGPointMake(self.player.size.width/2, self.frame.size.height/2);
         [self addChild:self.player];
         
     }
     return self;
+}
+
+-(void)addMonster {
+    SKSpriteNode * monster = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
+    
+    
+    int minY = monster.size.height / 2;
+    int maxY = self.frame.size.height - monster.size.height / 2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    monster.position = CGPointMake(self.frame.size.width + monster.size.width/2, actualY);
+    [self addChild:monster];
+    
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random()%rangDuration)+minDuration;
+    
+    SKAction * actionMove = [SKAction moveTo:CGPointMake(-monster.size.width/2, actualY) duration:actualDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    
+    [monster runAction:[SKAction sequence:@[actionMove,actionMoveDone]]];
+}
+
+- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
+    
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval > 1) {
+        self.lastSpawnTimeInterval = 0;
+        [self addMonster];
+        //NSLog(@"A new monster just added");
+    }
+}
+
+- (void)update:(NSTimeInterval)currentTime{
+    // Handle time delta.
+    // If we drop below 60fps, we still want everything to move the same distance.
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    if (timeSinceLast > 1) { // more than a second since last update
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+    
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
 }
 
 @end
